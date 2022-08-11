17f79c5553873dcca71ef72456edde8b4a3e1bfd
Casper Biering <casper@biering.dk>
2022-08-11 14:46:52 +0200

    chore: Add tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ['airbnb', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'jest'],
+  env: {
+    'jest/globals': true,
+  },
   rules: {
     'prettier/prettier': 'error',
   },
